d59cb7d6bff77b5c45ac5a90021f79c7f35048d3
AlexMapley <alexmapley@gmail.com>
2019-08-21 15:20:21 -0400

    switch to docker-esktop context if not already, add pretty printing

--- status_check.sh
+++ status_check.sh
@@ -1,2 +1,15 @@
+# Verify we're in docker-desktop
+printf "\n\n"
+kubectl config use-context docker-for-desktop
+kubectl config current-context
+
 # show kubectl version
+printf "\n\n"
 kubectl version
+
+# show all existing resources
+printf "\n\n"
+kubectl get all
+
+
+printf "\n\n"
