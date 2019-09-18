9adc9d29ee6f40f0dfb1880631e546b47c6950d7
AlexMapley <alexmapley@gmail.com>
2019-09-17 23:00:12 -0400

    more local mac ansible work

--- ansible/local_mac/mac_setup.sh
+++ ansible/local_mac/mac_setup.sh
@@ -1,5 +1,11 @@
-pip install ansible
+brew install ansible
 
 export ANSIBLE_HOSTS=/root/ansible_hosts
 ansible all --inventory-file=/root/ansible_hosts -m ping
 
+sudo mkdir /etc/ansible
+sudo cat hosts >> /etc/ansible/hosts
+
+ssh-keygen -t rsa -C "name@example.org"
+ssh-copy-id user@child1.dev
+ssh-copy-id user@child2.dev
