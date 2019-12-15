3bee53ab78af64669ac540271d10e33684c0fcdb
AlexMapley <alexmapley@gmail.com>
2019-12-15 10:03:51 -0500

    first recursion successs case

--- recursive_docker/recurse.sh
+++ recursive_docker/recurse.sh
@@ -1,11 +1,12 @@
 echo image layer: $1
-# docker build . -t "image_layer"$1
+docker build . -t "image_layer"$1
 
 image=$(docker images \
     | grep image_layer \
     | awk '{print $1}')
     
-echo $image
-echo "$(($1 + 1))"
+echo new image: $image
+new_layer="$(($1 + 1))"
+echo new layer: $new_layer
 
-docker run -ti $image /recurse.sh "$(($1 + 1))"
+docker run -ti $image /bin/sh -c "/recurse.sh $new_layer"
