a06ff7cde8f68b4ac05962d09c9c70820e37f9d0
AlexMapley <alexmapley@gmail.com>
2019-12-15 09:59:36 -0500

    debugging recusrion script

--- recursive_docker/recurse.sh
+++ recursive_docker/recurse.sh
@@ -1,10 +1,11 @@
 echo image layer: $1
-docker build . -t "image_layer"$1
+# docker build . -t "image_layer"$1
 
 image=$(docker images \
     | grep image_layer \
     | awk '{print $1}')
     
 echo $image
+echo "$(($1 + 1))"
 
-docker run $image /recurse.sh "$((a + 1))"
+docker run -ti $image /recurse.sh "$(($1 + 1))"
