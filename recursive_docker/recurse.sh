3b4a404468974c4dc7f8eee1b65e97ced7a255f6
AlexMapley <alexmapley@gmail.com>
2019-12-15 09:57:26 -0500

    update recurse.sh

--- recursive_docker/recurse.sh
+++ recursive_docker/recurse.sh
@@ -1,2 +1,10 @@
 echo image layer: $1
 docker build . -t "image_layer"$1
+
+image=$(docker images \
+    | grep image_layer \
+    | awk '{print $1}')
+    
+echo $image
+
+docker run $image /recurse.sh "$((a + 1))"
